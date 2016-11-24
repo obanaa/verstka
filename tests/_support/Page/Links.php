4fe7b60ceb2a98f17a48cf5d827a8eda33c4afca
Yuriy <yf@itsvit.org>
2016-11-24 16:08:22 +0200

    new

---
+++ tests/_support/Page/Links.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: obana
+ * Date: 24.11.16
+ * Time: 10:56
+ */
+
+#require_once('file.php');
+
+
+namespace Page;
+
+
+class Links
+{
+
+
+    protected $tester;
+
+    public function __construct(\AcceptanceTester $I)
+    {
+        $this->tester = $I;
+    }
+
+    public static $URL = '/';
+    public static $URL1 = '/blog';
+    public static $URL2 = '/about-it-svit';
+    public static $URL3 = '/our-services/devops/';
+    public static $URL4 = '/our-services/web-development/';
+    public static $URL5 = '/our-services/design/';
+    public static $URL6 = '/our-services/big-data-and-data-science/';
+    public static $URL7 = '/our-services/quality-assurance-and-automation/';
+    
+    public function pageAndScreen (){
+        $I = $this->tester;
+        $I->amOnPage(self::$URL1);
+        $I->scrollDown(1100);
+        $I->amOnPage(self::$URL2);
+        $I->scrollDown(1100);
+        $I->amOnPage(self::$URL3);
+        $I->scrollDown(1100);
+        $I->amOnPage(self::$URL4);
+        $I->scrollDown(1100);
+        $I->amOnPage(self::$URL5);
+        $I->scrollDown(1100);
+        $I->amOnPage(self::$URL6);
+        $I->scrollDown(1100);
+        $I->amOnPage(self::$URL7);
+        $I->scrollDown(1100);
+        }
+
+
+
+
+}
--- tests/acceptance/00_TestCest.php
+++ tests/acceptance/00_TestCest.php
@@ -6,69 +6,12 @@ use \Step\Acceptance;
  */
 class TestCest
 {
-/*
-    function T1Links( \Page\MainPage $mainPage)    {
-        $mainPage->headerLinks();
-        $mainPage->footerLinks();
-        $mainPage->socialFooterLinks();
-       // $mainPage->whatsNewBlock();
+
+    function T1Links( \Page\Links $links)    {
+        $links->pageAndScreen();
+
+        // $mainPage->whatsNewBlock();
     }
-*//**/
-    function T1HeaderLinks ( \Page\MainPage $mainPage){
-        $mainPage->headerLinks();  }
 
-    function T2FooterLinks( \Page\MainPage $mainPage) {
-        $mainPage->footerLinks();
-        $mainPage->socialFooterLinks();    }
-
-    function T3MainPage( \Page\MainPage $mainPage){
-     //   $mainPage->sliderBlock();
-        $mainPage->whoWeAreSlider();
-        $mainPage->ourStrengthsBlock();
-        $mainPage->whatsNewBlock();
-        $mainPage->ourPartnersSlider();     }
-
-    function T4OurServicesPage( \Page\OurServicesPage $ourServicesPage) {
-        $ourServicesPage->pageNavigation();     }
-
-    function T5AboutUsPage( \Page\AboutUsPage $aboutUsPage) {
-        $aboutUsPage->aboutUsInstagramBlock(); }
-
-    function T6ValidAndInvalidDataOnContactPage  ( \Page\ContactsPage $contactsPage) {
-        $contactsPage->invalidLeaveAMessageData('INVALID');
-        $contactsPage->validLeaveAMessageData('Test','test@test.com','test message'); }
-
-    function T7DevOpsPage( \Page\DevOpsPage $devOpsPage) {
-        $devOpsPage->devOpsFeatures();      }
-
-    function T8WebDevelopmentPage( \Page\WebDevPage $webDevPage){
-        $webDevPage ->webDevFeatures();     }
-
-    function T9DesignPage (\Page\DesignPage $designPage ){
-        $designPage->designFeatures();    }
-
-    function T10QualityAssurancePage (\Page\QualityAssurancePage $qualityAssurancePage ){
-        $qualityAssurancePage->qualityAssurancePageFeatures();    }
-
-    function T11DocumentationPage (\Page\DocumentationPage $documentationPage ){
-        $documentationPage->documentationFeatures();    }
-
-    function T12BigDataPage   (\Page\BigDataPage $bigDataPage ){
-        $bigDataPage->bigDataFeatures();    }
-
-    function T13BlogPage (\Page\BlogPage $blogPage){
-        $blogPage->pageNavigation();
-        $blogPage->checkBlocks();    }
-
-    function T14Portfolio ( \Page\PortfolioPage $portfolioPage)    {
-        $portfolioPage->portfolioTabs();
-        $portfolioPage->webDevelopmentTabs();
-        $portfolioPage->qaTabs();
-        $portfolioPage->documentationTabs();
-        $portfolioPage->devOpsTabs();
-        $portfolioPage->designTabs();
-        $portfolioPage->bigDataTabs();    }
-
-/**/
 }
 
